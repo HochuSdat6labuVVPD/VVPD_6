3a47b93467f2705ee39f9864871f3ce1d8d5b391
DenisK <denisknyazkov2003@mail.ru>
2021-12-15 21:52:32 +0700

    Добавлен файл с алгоритмом и создан файл для тестов

---
+++ main.py
@@ -0,0 +1,66 @@
+"""Князьков Денис Алексеевич КИ21-17/1б
+Практическая работа №4, 11 вариант"""
+
+
+def is_full_connected(v, r, links):
+    if r == (v * (v - 1)):
+        return True
+    else:
+        return False
+
+
+def connection_type(v, r, links):
+    dict_of_links = {}
+    for x in links:
+        if x[0] in dict_of_links:
+            dict_of_links[x[0]].add(x[1])
+        else:
+            dict_of_links[x[0]] = {x[1]}
+        if x[1] in dict_of_links:
+            dict_of_links[x[1]].add(x[0])
+        else:
+            dict_of_links[x[1]] = {x[0]}
+    flag = 0
+    for x in dict_of_links:
+        if len(dict_of_links[x]) == 1:
+            continue
+        elif len(dict_of_links[x]) == r:
+            flag = 1
+        else:
+            break
+    if flag:
+        return 3
+    counter = 0
+    for x in dict_of_links:
+        if len(dict_of_links[x]) == 1:
+            counter += 1
+        elif len(dict_of_links[x]) == 2:
+            continue
+        else:
+            return -1
+    else:
+        if counter == 0:
+            return 2
+        elif counter == 2:
+            return 1
+        else:
+            return -1
+
+
+print("1 - Шина, 2 - Кольцо, 3 - Звезда, -1 - Не относится ни к одному из данных типов ")
+first = [(1, 2), (1, 3), (1, 4), (2, 1), (2, 3), (2, 4), (3, 1), (3, 2), (3, 4), (4, 1), (4, 2),
+         (4, 3)]  # Сеть является полносвязной
+print(is_full_connected(4, 12, first))
+print(connection_type(4, 12, first))
+second = [(1, 2), (2, 3), (3, 4), (4, 5), (1, 5)]  # Сеть является неполносвязной, кольцо
+print(is_full_connected(5, 5, second))
+print(connection_type(5, 5, second))
+third = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]  # Сеть является неполносвязной, шина
+print(is_full_connected(6, 5, third))
+print(connection_type(6, 5, third))
+fourth = [(1, 2), (1, 3), (1, 4), (1, 5)]  # Сеть является неполносвязной, звезда
+print(is_full_connected(5, 4, fourth))
+print(connection_type(5, 4, fourth))
+fifth = [(1, 2), (1, 3), (1, 4), (2, 1), (2, 3)]  # Сеть является неполносвязной и не относится ни к одному из типов
+print(is_full_connected(4, 5, fifth))
+print(connection_type(4, 5, fifth))
