86d99ee13b59a3cf56cf051c86ea5a5e2f0659bf
DenisK <denisknyazkov2003@mail.ru>
2021-12-18 23:25:22 +0700

    Написаны тесты для функции connection_type

---
+++ test_.py
@@ -0,0 +1,32 @@
+from main import is_full_connected
+from main import connection_type
+import pytest
+
+
+@pytest.mark.parametrize("v, r, links", [(4, 12, [(1, 2), (3, 4)]), (3, 6, [(1, 3)]), (5, 20, [(1, 2), (1, 3)])])
+def test_is_full_connected(v, r, links):
+    assert is_full_connected(v, r, links) is True
+
+
+@pytest.mark.parametrize("v, r, links",
+                         [(6, 5, [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]), (4, 3, [(1, 2), (2, 3), (3, 4)])])
+def test_connection_type_1(v, r, links):
+    assert connection_type(v, r, links) == 1
+
+
+@pytest.mark.parametrize("v, r, links",
+                         [(5, 5, [(1, 2), (2, 3), (3, 4), (4, 5), (1, 5)]), (3, 3, [(1, 2), (2, 3), (3, 1)])])
+def test_connection_type_2(v, r, links):
+    assert connection_type(v, r, links) == 2
+
+
+@pytest.mark.parametrize("v, r, links", [(5, 4, [(1, 2), (1, 3), (1, 4), (1, 5)]), (4, 3, [(1, 2), (1, 3), (1, 4)])])
+def test_connection_type_3(v, r, links):
+    assert connection_type(v, r, links) == 3
+
+
+@pytest.mark.parametrize("v, r, links",
+                         [(4, 5, [(1, 2), (1, 3), (1, 4), (2, 1), (2, 3)]), (4, 4, [(1, 2), (1, 3), (2, 3), (2, 4)]),
+                          (4,6,[(1,2),(1,3),(1,4),(3,4),(2,3)])])
+def connection_type_4(v, r, links):
+    assert connection_type(v, r, links) == -1
